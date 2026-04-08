637f4325c17b537dddc782efd286aa960e4f5606
Prakash Pandey <153199860+PrakashPandit1@users.noreply.github.com>
2026-04-08 13:15:34 +0530

    Add files via upload

---
+++ src/containers/about/index.jsx
@@ -0,0 +1,114 @@
+import { BsInfoCircleFill } from "react-icons/bs";
+import { DiAndroid, DiApple } from "react-icons/di";
+import { FaDatabase, FaDev } from "react-icons/fa";
+import { Animate } from "react-simple-animate";
+import PageHeaderContent from "../../components/pageHeaderContent";
+import "./styles.scss";
+
+const personalDetails = [
+  {
+    label: "Name",
+    value: "Prakash Pandey",
+  },
+  {
+    label: "Age",
+    value: "27",
+  },
+  {
+    label: "Address",
+    value: "India",
+  },
+  {
+    label: "Email",
+    value: "prakashpandeynewid@gmail.com",
+  },
+  {
+    label: "Contact No",
+    value: "+91 9621023012",
+  },
+];
+
+const jobSummary =
+  "Seasoned and independent Front End Developer with 1 years of experience in blending the art of design with skill of programming to deliver an immersive and engaging user experience through efficient website development, proactive feature optimization, and relentless debugging. Very passionate about aesthetics and UI design. It is imperative that you provide a thorough and professional approach to your resume. As a Front End Developer you will be judged by your ability to use UX and UI concepts and follow design guidelines. It is about expressing your attention to detail and how you can help implement design ideas for your future employer";
+
+const About = () => {
+  return (
+    <section id="about" className="about">
+      <PageHeaderContent
+        headerText="About Me"
+        icon={<BsInfoCircleFill size={40} />}
+      />
+      <div className="about__content">
+        <div className="about__content__personalWrapper">
+          <Animate
+            play
+            duration={1.5}
+            delay={1}
+            start={{
+              transform: "translateX(-900px)",
+            }}
+            end={{
+              transform: "translatex(0px)",
+            }}
+          >
+            <h3>Front End Developer</h3>
+            <p>{jobSummary}</p>
+          </Animate>
+
+          <Animate
+            play
+            duration={1.5}
+            delay={1}
+            start={{
+              transform: "translateX(500px)",
+            }}
+            end={{
+              transform: "translatex(0px)",
+            }}
+          >
+            <h3 className="personalInformationHeaderText">
+              Personal Information
+            </h3>
+            <ul>
+              {personalDetails.map((item, i) => (
+                <li key={i}>
+                  <span className="title">{item.label}</span>
+                  <span className="value">{item.value}</span>
+                </li>
+              ))}
+            </ul>
+          </Animate>
+        </div>
+        <div className="about__content__servicesWrapper">
+          <Animate
+            play
+            duration={1.5}
+            delay={1}
+            start={{
+              transform: "translateX(600px)",
+            }}
+            end={{
+              transform: "translatex(0px)",
+            }}
+          >
+            <div className="about__content__servicesWrapper__innerContent">
+              <div>
+                <FaDev size={60} color="var( --yellow-theme-main-color)" />
+              </div>
+              <div>
+                <DiAndroid size={60} color="var( --yellow-theme-main-color)" />
+              </div>
+              <div>
+                <FaDatabase size={60} color="var( --yellow-theme-main-color)" />
+              </div>
+              <div>
+                <DiApple size={60} color="var( --yellow-theme-main-color)" />
+              </div>
+            </div>
+          </Animate>
+        </div>
+      </div>
+    </section>
+  );
+};
+export default About;
---
+++ src/containers/resume/utils.js
@@ -0,0 +1,42 @@
+export const data = {
+    certificates: [
+      {
+        title: "NCC",
+        subTitle: "A",
+        
+          
+      },
+      {
+        title: "Computer System Security",
+        subTitle: "IIT Kanpur, INDIA",
+        description:
+          "Web development, Databases, Backend developement",
+      },
+      {
+        title: "ICNGTSE 2025 ",
+        subTitle: "PSIT Kanpur, INDIA",
+        description:
+          "Exploring The Quantum Computing",
+      },
+    ],
+    education: [
+      {
+        title: "Higher Secondary",
+        subTitle: " TNJIC BANKATA",
+        description:
+          "12th Grade in Science",
+      },
+      {
+        title: "B.Tech",
+        subTitle: "GNIOT GREATER NOIDA",
+        description:
+          "Bachelor of technology in computer science",
+      },
+      {
+        title: "M.Tech",
+        subTitle: "MMMUT GORAKHPUR",
+        description:
+          "Master of technology in computer science",
+      },
+    ],
+  };
